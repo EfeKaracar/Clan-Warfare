7a44022ba72e944bb34aa5b1945fe7ec850c188c
EfeKaracar <efekaracar@gmail.com>
2021-04-02 02:52:21 -0700

    modull info stupidity
    modull info stupidity

--- module_info.py
+++ module_info.py
@@ -1,6 +1,10 @@
 
-# Point export_dir to the folder you will be keeping your module
-# Make sure you use forward slashes (/) and NOT backward slashes (\)
 
+
+
+# Belendor
 export_dir = "C:/Program Files (x86)/Steam/steamapps/common/MountBlade Warband/Modules/Clans Warfare/"
-#export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/"
+
+# Cedric
+#export_dir = "D:/Mount&Blade Warband/Modules/Clan Warfare/"
+
