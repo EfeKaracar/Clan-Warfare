9d35483802b9f68256ad3e28eeacbab5f1710215
CdJrcxx <81741616+CdJrcxx@users.noreply.github.com>
2021-04-16 20:40:04 -0500

    small changes

--- module_troops.py
+++ module_troops.py
@@ -472,11 +472,11 @@ troops = [
    def_attrib|level(4),wp(60),knows_common,swadian_face_younger_1, swadian_face_middle_2],
   
   ["swadian_militia","Sword of Discord","Swords of Discord",tf_guarantee_boots|tf_guarantee_armor|tf_guarantee_shield,0,0,fac_kingdom_1,
-   [itm_sword_medieval_a, itm_tab_shield_heater_a, itm_boar_spear, itm_padded_cloth, itm_red_gambeson, itm_arming_cap, itm_ankle_boots, itm_wrapping_boots],
+   [itm_sword_medieval_a, itm_tab_shield_heater_a, itm_boar_spear, itm_padded_cloth,itm_fighting_pick, itm_red_gambeson, itm_arming_cap, itm_ankle_boots, itm_wrapping_boots],
    def_attrib|level(9),wp(75),knows_common,swadian_face_young_1, swadian_face_old_2],
   
   ["swadian_footman","Rat Nobility","Rat Nobility",tf_guarantee_boots|tf_guarantee_armor|tf_guarantee_shield,0,0,fac_kingdom_1,
-   [itm_war_spear,itm_warhammer,itm_sword_medieval_b_small,itm_sword_medieval_a,itm_tab_shield_heater_b,
+   [itm_war_spear,itm_military_hammer,itm_military_pick,itm_sword_medieval_b_small,itm_sword_medieval_a,itm_tab_shield_heater_b,
     itm_mail_with_tunic_red,itm_leather_boots,itm_mail_coif,itm_norman_helmet, itm_mail_mittens],
    def_attrib|level(16),wp_melee(115),knows_common|knows_ironflesh_3|knows_shield_3|knows_athletics_3|knows_power_strike_3,swadian_face_young_1, swadian_face_old_2],
   
@@ -561,10 +561,10 @@ troops = [
    [itm_sword_viking_2, itm_strong_bow, itm_barbed_arrows, itm_barbed_arrows, itm_war_bow, 
     itm_mail_hauberk,itm_lamellar_vest,itm_leather_boots,itm_vaegir_lamellar_helmet,itm_vaegir_spiked_helmet],
    def_attrib|level(28),wp_melee(270),knows_athletics_6|knows_ironflesh_5|knows_power_strike_4|knows_power_draw_7|knows_shield_2,vaegir_face_young_1, vaegir_face_older_2], 
-  ["vaegir_guard","Saddle Horseman","Saddle Horsemen",tf_mounted|tf_guarantee_shield|tf_guarantee_boots|tf_guarantee_armor|tf_guarantee_helmet,0,0,fac_kingdom_2,
+  ["vaegir_guard","Saddle Horseman","Saddle Horsemen",tf_mounted|tf_guarantee_shield|tf_guarantee_horse|tf_guarantee_boots|tf_guarantee_armor|tf_guarantee_helmet,0,0,fac_kingdom_2,
    [itm_saddle_horse,itm_lance,itm_sword_medieval_b,itm_tab_shield_kite_d,
     itm_studded_leather_coat,itm_mail_boots,itm_vaegir_lamellar_helmet,itm_leather_gloves],
-   def_attrib|level(16),wp_melee(100),knows_riding_2|knows_athletics_4|knows_shield_2|knows_ironflesh_2|knows_power_strike_4,vaegir_face_middle_1, vaegir_face_older_2],
+   def_attrib|level(16),wp_melee(130),knows_riding_2|knows_athletics_4|knows_shield_2|knows_ironflesh_2|knows_power_strike_4,vaegir_face_middle_1, vaegir_face_older_2],
   ["vaegir_horseman","Man of Cow","Men of Cow",tf_mounted|tf_guarantee_boots|tf_guarantee_armor|tf_guarantee_gloves|tf_guarantee_helmet|tf_guarantee_horse|tf_guarantee_shield,0,0,fac_kingdom_2,
    [itm_sword_medieval_d_long,itm_morningstar,itm_heavy_lance,itm_awlpike_long,itm_steel_shield,
     itm_lamellar_armor,itm_scale_gauntlets,itm_vaegir_noble_helmet, itm_vaegir_war_helmet, itm_vaegir_mask, itm_warhorse_steppe, itm_iron_greaves, itm_mail_chausses, itm_scale_gauntlets, itm_mail_mittens],
@@ -1198,7 +1198,7 @@ troops = [
   ["kingdom_heroes_including_player_begin",  "kingdom_heroes_including_player_begin",  "kingdom_heroes_including_player_begin",  tf_hero, 0,reserved,  fac_kingdom_1,[],          lord_attrib,wp(220),knows_lord_1, 0x000000000010918a01f248377289467d],
 
   ["npc1","Cru","Cru",tf_hero|tf_unmoveable_in_party_window, 0, reserved, fac_commoners,[itm_khergit_armor,itm_nomad_boots,itm_saddle_horse],
-   str_8|agi_7|int_12|cha_7|level(3),wp(60),knows_tracker_npc|
+   str_8|agi_7|int_12|cha_7|level(20),wp(200),knows_tracker_npc|
    knows_ironflesh_1|knows_power_strike_1|knows_pathfinding_3|knows_athletics_2|knows_tracking_1|knows_riding_2, #skills 2/3 player at that level
    0x00000004bf086143259d061a9046e23500000000001db52c0000000000000000],
   ["npc2","Zaffa","Zaffa", tf_hero|tf_unmoveable_in_party_window, 0,reserved, fac_commoners,[itm_linen_tunic,itm_hide_boots,itm_club],
@@ -3135,11 +3135,11 @@ upgrade2(troops,"nord_veteran","nord_champion", "nord_huntsman")
 
 
 #House Brightfyre upgrade declarations
-upgrade2(troops,"vaegir_recruit","vaegir_footman","vaegir_guard")
+upgrade(troops,"vaegir_recruit","vaegir_footman")
 upgrade2(troops,"vaegir_footman","vaegir_skirmisher","vaegir_veteran")
 upgrade(troops,"vaegir_guard","vaegir_horseman")
 upgrade(troops,"vaegir_skirmisher","vaegir_archer")
-upgrade(troops,"vaegir_archer","vaegir_marksman")
+upgrade2(troops,"vaegir_archer","vaegir_marksman","vaegir_guard")
 upgrade(troops,"vaegir_veteran","vaegir_infantry")
 
 
